976d9d8223844983629be3bfdc3bf012eaf585e3
CrazyDany <konevdanil789@gmail.com>
2026-05-23 17:45:54 +0300

    Деятельность малых хвостов
    - Полностью переделана система и способность SmallTall
    --- SmallTall теперь меняет хитбоксы более коректно
    --- SmallTall теперь синхронизирует свои эффекты с другими клиентами

--- ability-smalltall.lua
+++ ability-smalltall.lua
@@ -1,51 +1,80 @@
 ABILITY_ID_SMALLTALL = 6
 
 local function onUseSmallTall()
-    AbilitiesData[ABILITY_ID_SMALLTALL].STTimer = 512
-    if random_float() >= 0.5 then
-        AbilitiesData[ABILITY_ID_SMALLTALL].scale = 3.0
-    else
-        AbilitiesData[ABILITY_ID_SMALLTALL].scale = 0.2
-    end
+    gPlayerSyncTable[0].Kaisen64.smalltall_size = 2 ^ random_sign()
+    gPlayerSyncTable[0].Kaisen64.smalltall_cur_duration = 256
+
+    local m = gMarioStates[0]
+
+    m.marioObj.hitboxRadius = m.marioObj.hitboxRadius * gPlayerSyncTable[0].Kaisen64.smalltall_size
 end
 
-hook_event(HOOK_MARIO_UPDATE, function(m)
-    if m.playerIndex ~= 0 then return end
+hook_event(HOOK_MARIO_UPDATE,
+    --- comment
+    --- @param m MarioState
+    function(m)
+        if gPlayerSyncTable[m.playerIndex].Kaisen64 == nil then return end
+        if ((gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_cur_duration or 0) > 0) then
+            if m.playerIndex == 0 then
+                gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_cur_duration = gPlayerSyncTable[m.playerIndex]
+                    .Kaisen64
+                    .smalltall_cur_duration - 1
+            end
 
-    if AbilitiesData[ABILITY_ID_SMALLTALL].STTimer > 1 then
-        obj_scale(m.marioObj, AbilitiesData[ABILITY_ID_SMALLTALL].scale)
-        obj_set_hitbox_radius_and_height(m.marioObj, 37 * AbilitiesData[ABILITY_ID_SMALLTALL].scale, 160 * AbilitiesData[ABILITY_ID_SMALLTALL].scale)
-        AbilitiesData[ABILITY_ID_SMALLTALL].STTimer = AbilitiesData[ABILITY_ID_SMALLTALL].STTimer - 1
-        if AbilitiesData[ABILITY_ID_SMALLTALL].scale == 3.0 then
-            set_override_fov(65)
-            if (m.hurtCounter) ~= 0 then
-                m.hurtCounter = m.hurtCounter / 2
+            m.marioObj.header.gfx.scale.x = m.marioObj.header.gfx.scale.x *
+                gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size
+
+            m.marioObj.header.gfx.scale.z = m.marioObj.header.gfx.scale.z *
+                gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size
+
+            m.marioObj.header.gfx.scale.y = m.marioObj.header.gfx.scale.y *
+                gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size
+
+            m.marioObj.hitboxHeight = m.marioObj.hitboxHeight *
+                gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size
+
+            if (gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size > 1) then
+                if m.playerIndex == 0 then
+                    set_override_fov(65)
+                end
+
+                if m.action == ACT_WALKING then
+                    smlua_anim_util_set_animation(m.marioObj, "mario_anim_walking")
+                    set_camera_shake_from_hit(SHAKE_GROUND_POUND)
+                    play_step_sound(m, 0, 88)
+                end
             end
-            if (get_mario_stick_input(m)) and not (get_mario_turn_around(m)) then
-                mario_set_forward_vel(m, math.clamp(m.forwardVel, -25, 25))
+
+            if (gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size < 1) then
+                if m.playerIndex == 0 then
+                    set_override_fov(35)
+                end
             end
-            if m.action == ACT_WALKING then
-                smlua_anim_util_set_animation(m.marioObj, "mario_anim_walking")
-                play_step_sound(m, 0, 88)
+            if (ACT_FLAG_AIR and (m.vel.y > 0)) ~= 0 then
+                m.vel.y = m.vel.y - (gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size or 0) / 2
             end
-            if AbilitiesData[ABILITY_ID_SMALLTALL].actionsShake[m.action] ~= nil then
-                set_camera_shake_from_hit(SHAKE_GROUND_POUND)
-            end
-        elseif AbilitiesData[ABILITY_ID_SMALLTALL].scale == 0.2 then
-            set_override_fov(35)
-            if (m.hurtCounter) ~= 0 then
-                m.hurtCounter = m.hurtCounter * 1.5
-            end
-            if (get_mario_stick_input(m)) and not (get_mario_turn_around(m)) then
-                mario_set_forward_vel(m, math.clamp(m.forwardVel * 1.15, -75, 75))
+            if (gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_cur_duration == 0) then
+                m.marioObj.hitboxRadius = m.marioObj.hitboxRadius / gPlayerSyncTable[0].Kaisen64.smalltall_size
+                gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size = 1
+                if m.playerIndex == 0 then
+                    set_override_fov(0)
+                end
             end
         end
-    elseif AbilitiesData[ABILITY_ID_SMALLTALL].STTimer == 1 then
-        set_override_fov(50)
-        AbilitiesData[ABILITY_ID_SMALLTALL].STTimer = 0
-        obj_set_hitbox_radius_and_height(m.marioObj, 37, 160)
     end
-end)
+)
+
+hook_event(HOOK_BEFORE_PHYS_STEP,
+    --- @param m MarioState
+    --- @param s integer
+    function(m, s)
+        if gPlayerSyncTable[m.playerIndex].Kaisen64 == nil then return end
+        if ((gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_cur_duration or 0) > 0) then
+            m.vel.x = m.vel.x / ((gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size or 1) * 0.75)
+            m.vel.z = m.vel.z / ((gPlayerSyncTable[m.playerIndex].Kaisen64.smalltall_size or 1) * 0.75)
+        end
+    end
+)
 
 RegisterAbility(ABILITY_ID_SMALLTALL, {
     name = "SmallTall",
@@ -65,25 +94,17 @@ RegisterAbility(ABILITY_ID_SMALLTALL, {
         return { " - " }
     end,
 
-    STTimer = 0,
+    onResetVariables = function()
+        if gPlayerSyncTable[0].Kaisen64 == nil then return end
 
-    scale = 0,
+        local m = gMarioStates[0]
 
-    actionsShake = {
-        [ACT_WALKING] = true,
-        [ACT_JUMP_LAND] = true,
-        [ACT_DOUBLE_JUMP_LAND] = true,
-        [ACT_TRIPLE_JUMP_LAND] = true,
-        [ACT_LONG_JUMP_LAND] = true,
-        [ACT_SIDE_FLIP_LAND] = true,
-        [ACT_BACKFLIP_LAND] = true,
-        [ACT_SOFT_BONK] = true,
-        [ACT_SOFT_BACKWARD_GROUND_KB] = true,
-        [ACT_BACKWARD_GROUND_KB] = true,
-        [ACT_HARD_BACKWARD_GROUND_KB] = true,
-        [ACT_SOFT_FORWARD_GROUND_KB] = true,
-        [ACT_FORWARD_GROUND_KB] = true,
-        [ACT_HARD_FORWARD_GROUND_KB] = true,
-        [ACT_GROUND_POUND_LAND] = true,
-    }
+        m.marioObj.hitboxRadius = m.marioObj.hitboxRadius / (gPlayerSyncTable[0].Kaisen64.smalltall_size or 1)
+
+        gPlayerSyncTable[0].Kaisen64.smalltall_size = 1
+        gPlayerSyncTable[0].Kaisen64.smalltall_cur_duration = 0
+    end,
+
+    --- custom fields
+    smalltall_duration = 256
 })
