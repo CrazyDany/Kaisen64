7bbca1b3a96c68f0777f0b3a482af9de093e55fd
CrazyDany <konevdanil789@gmail.com>
2026-05-27 19:51:42 +0300

    Update object_gojosblue.lua
    - Теперь синий (Gojos Blue) притягивает не только игроков, но и любые объекты

--- object_gojosblue.lua
+++ object_gojosblue.lua
@@ -79,8 +79,6 @@ hook_event(HOOK_BEFORE_PHYS_STEP,
     end
 )
 
-id_bhvGojosBlue = hook_behavior(nil, OBJ_LIST_DEFAULT, false, bhv_GojosBlue_init, bhv_GojosBlue_loop)
-
 hook_event(HOOK_MARIO_UPDATE,
     function(m)
         if gPlayerSyncTable[m.playerIndex].Kaisen64 == nil then return end
@@ -92,5 +90,59 @@ hook_event(HOOK_MARIO_UPDATE,
         end
     end
 )
+
+
+local function obj_override_behavior_script(o, script)
+    if obj_has_behavior_id(o, id_bhvGojosBlue) ~= 0 then return end
+
+    local closest_blue = obj_get_nearest_object_with_behavior_id(o, id_bhvGojosBlue)
+
+    if closest_blue == nil then return end
+
+    local dist = dist_between_objects(o, closest_blue)
+    local blue_raidus = closest_blue.oAttractRadius or 0
+    local blue_strength = closest_blue.oAttractStrength or 0
+    local blue_lapse_radius = closest_blue.oLapseRadius or 0
+
+    if dist <= blue_raidus then
+        local dx = closest_blue.oPosX - o.oPosX
+        local dy = closest_blue.oPosY - o.oPosY
+        local dz = closest_blue.oPosZ - o.oPosZ
+
+        local dirX = dx / dist
+        local dirY = dy / dist
+        local dirZ = dz / dist
+
+        local strength = blue_strength * (1 - dist / blue_raidus)
+
+        obj_move_xyz(o, dirX * strength, dirY * strength, dirZ * strength)
+
+        -- if dist <= blue_lapse_radius then
+        --     -- m.health = m.health - 1
+        --     if closest_blue.oForwardVel > closest_blue.oForwardVelAfterHit then
+        --         closest_blue.oForwardVel = math.floor(math.lerp(closest_blue.oForwardVel,
+        --         closest_blue.oForwardVelAfterHit, 0.05))
+        --         closest_blue.oTimer = closest_blue.oTimer + 1
+        --     end
+        -- end
+    end
+end
+
+hook_event(HOOK_UPDATE, function()
+    local sObjList = {}
+    -- Inserts all object lists automatically
+    for k, v in pairs(_G) do
+        if not k:find("OBJ_LIST_") then goto continue end
+        table.insert(sObjList, v)
+        ::continue::
+    end
+    for _, objList in pairs(sObjList) do
+        local obj = obj_get_first(objList)
+        while obj ~= nil do
+            obj_override_behavior_script(obj, get_behavior_from_id(id_bhvGoomba))
+            obj = obj_get_next(obj)
+        end
+    end
+end)
 
 id_bhvGojosBlue = hook_behavior(nil, OBJ_LIST_DEFAULT, false, bhv_GojosBlue_init, bhv_GojosBlue_loop)
